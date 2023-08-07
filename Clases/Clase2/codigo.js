5b1a2711c383ee675291177a11c73815b3e436dc
JFEMENIA <jorgefemenia@gmail.com>
2023-08-07 20:29:37 -0300

    clase2 interacciones js

--- Clases/Clase2/codigo.js
+++ Clases/Clase2/codigo.js
@@ -6,10 +6,10 @@ let confirmacion = confirm(`Ingresaste ${nombre}, continuar?`);
 
 if (confirmacion == true){
     alert ("Bienvenido");
+    // mostrar variable en el front
+    document.write(`Hola ${nombre}`);
 }
 else {
     alert ("La próxima ingresa bien tu nombre");
+    document.write(`Recarga la pagina para llenar el formulario de nuevo`);
 }
-
-// mostrar variable en el front
-document.write(`Hola ${nombre}`);
