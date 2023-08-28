813f63fce4be517411560ce0b0ce46de55f0d339
JFEMENIA <jorgefemenia@gmail.com>
2023-08-28 19:31:32 -0300

    clase 7

---
+++ Clases/Clase7/codigo.js
@@ -0,0 +1,21 @@
+// estructuras de control
+
+let numero = Number(prompt("Decime un número: "));
+
+// While
+/*
+while(numero < 1000){    
+    document.write(numero + "<br>")
+    numero++;
+} */
+
+// do while
+/* do {
+    document.write(numero + "<br>")
+    numero++;
+} while(numero < 10); */
+
+// For
+for(let i = numero;i<10;i++){
+    document.write(i)
+}
