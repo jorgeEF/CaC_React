e96cbce982009e0d71925865308120fe376eb65f
JFEMENIA <jorgefemenia@gmail.com>
2023-09-27 20:38:48 -0300

    clase 13 vite-app

---
+++ Clases/Clase13_vite-app/src/Boton.jsx
@@ -0,0 +1,13 @@
+
+function Boton (props) {
+
+    // codigo de parte lógica
+
+    return(
+        // parte que se renderiza
+        // eslint-disable-next-line react/prop-types
+        <button>{props.numero}</button>
+    )
+}
+
+export default Boton;
--- Clases/Clase13_vite-app/src/main.jsx
+++ Clases/Clase13_vite-app/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import Boton from './Boton.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode>    
+    <Boton numero = {1}/>
+    <Boton numero = {2}/>
+    <Boton numero = {3}/>
+    <Boton numero = {4}/>
+    <Boton numero = {5}/>
   </React.StrictMode>,
 )
