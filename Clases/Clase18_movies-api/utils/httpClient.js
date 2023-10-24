25f7160ae63fe6cf470788d649bfe09ca353bb3d
JFEMENIA <jorgefemenia@gmail.com>
2023-10-24 13:33:49 -0300

    agregado manejo api key en .env clase 18 y 19

--- Clases/Clase18_movies-api/utils/httpClient.js
+++ Clases/Clase18_movies-api/utils/httpClient.js
@@ -1,10 +1,11 @@
 const API = "https://api.themoviedb.org/3/"
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 export const get = (path) => {
 
     return fetch (API+path,{
         headers: {        
-        Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZjM0YzBmZDc4NjQ4N2RkMTg0OWM4NjVkNjQyZTJlOCIsInN1YiI6IjVjNzcyZjBkMGUwYTI2NWU2YzU3NTFiNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gCzF1CXH5rdYq-Ufx_i4zv1C3OA5FuEKxCEOU5xv-m4",
+        Authorization: `Bearer ${API_KEY}`,
         "Content-Type": "application/json;charset=utf-8"
         }
     }).then((results)=>results.json())
