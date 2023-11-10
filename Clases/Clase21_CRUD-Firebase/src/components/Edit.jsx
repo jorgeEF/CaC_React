9c68c705f174f9a313ce71850281ef17cd53735b
JFEMENIA <jorgefemenia@gmail.com>
2023-11-10 14:13:36 -0300

    clases reorganizadas

--- Clases/Clase21_CRUD-Firebase/src/components/Edit.jsx
+++
@@ -1,6 +0,0 @@
-export const Edit = () => {
-
-    return (
-        <h1>Aca se van a editar los documentos</h1>
-    )
-}
--- Clases/Clase23_CRUD-Firestore/src/components/Create.jsx
+++
@@ -1,6 +0,0 @@
-export const Create = () => {
-
-    return (
-        <h1>Aca se van a crear documentos nuevos</h1>
-    )
-}
